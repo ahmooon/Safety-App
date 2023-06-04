2837b2277d80a12281c5f4987849f4ef2b9e66d0
ahmooon <132542696+ahmooon@users.noreply.github.com>
2023-06-04 19:02:06 +0900

    Initial Commit

---
+++ Safety App/Safety_AppApp.swift
@@ -0,0 +1,20 @@
+//
+//  Safety_AppApp.swift
+//  Safety App
+//
+//  Created by Hojin Moon on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Safety_AppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
